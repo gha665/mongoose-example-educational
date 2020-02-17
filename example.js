aad0b9d34d8528eb70f071ceeed73e4658da1cfc
Evgeny Galitskiy <gha665@gmail.com>
2020-02-17 00:37:42 -0500

    first commit

---
+++ example.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/exampleApp', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+
+
+  const Cat = mongoose.model('Cat', { name: String });
