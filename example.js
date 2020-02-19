69b97c5ffee61f766cfbd41dd8d0c7ffaa5c33bc
Evgeny Galitskiy <gha665@gmail.com>
2020-02-18 23:43:18 -0500

    updated code

--- example.js
+++ example.js
@@ -10,5 +10,44 @@ mongoose
   .catch(err => console.error('Error connecting to mongo', err));
 
 
+//First model
+const Cat = mongoose.model('Cat', {
+  name: String
+});
 
-  const Cat = mongoose.model('Cat', { name: String });
+
+//Create a new instance
+const kitty = new Cat({
+  name: 'Ironhacker'
+});
+
+
+//Save kitty
+kitty
+  .save()
+  .then(newCat => console.log(`A new cat is created ${newCat}`))
+  .catch(err => console.log(`Error while creating a new cat: ${err}`));
+
+
+function showCats() {
+  console.log('All the CATS!');
+  //List all cats
+  Cat.find()
+    .then(catsFromDB => {
+      catsFromDB.forEach(oneCat => console.log(`--> cat: ${oneCat.name}`))
+    })
+    .catch(err => console.log(`Error occurred during getting cats from DB: ${err}`));
+};
+
+
+function addTenCats() {
+  for (let i = 0; i < 10; i++) {
+    addNewCat(`Ironhacker ${i}`);
+  }
+};
+
+addTenCats();
+
+/* We have to wait for our cats to save before displaying them
+ Remember, it's async */
+setTimeout(showCats, 1500);
